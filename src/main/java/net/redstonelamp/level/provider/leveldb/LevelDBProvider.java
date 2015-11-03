c17a8431a55b2713a7097786698b0ecf663bd3b7
Trent <trent0308@live.com>
2015-11-03 17:17:43 -0600

    Revert that nasty old fix

--- src/main/java/net/redstonelamp/level/provider/leveldb/LevelDBProvider.java
+++ src/main/java/net/redstonelamp/level/provider/leveldb/LevelDBProvider.java
@@ -70,7 +70,7 @@ public class LevelDBProvider implements LevelProvider{
         options.createIfMissing(true);
         options.compressionType(CompressionType.ZLIB);
 
-        File databaseDir = new File(levelDir + File.separator + "data");
+        File databaseDir = new File(levelDir + File.separator + "db");
         try{
             database = Iq80DBFactory.factory.open(databaseDir, options);
         }catch(IOException e){
