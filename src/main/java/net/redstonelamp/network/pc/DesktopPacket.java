4fa48301b68a466a50e11f7a7d9de89044a32d91
PEMapModder <pemapmodder1970@gmail.com>
2015-11-11 17:51:13 +0800

    Fixed license headers

--- src/main/java/net/redstonelamp/network/pc/DesktopPacket.java
+++ src/main/java/net/redstonelamp/network/pc/DesktopPacket.java
@@ -1,10 +1,26 @@
+/*
+ * This file is part of RedstoneLamp.
+ *
+ * RedstoneLamp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RedstoneLamp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RedstoneLamp.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.redstonelamp.network.pc;
-
-import java.net.SocketAddress;
-import java.nio.ByteOrder;
 
 import net.redstonelamp.network.UniversalPacket;
 import net.redstonelamp.nio.BinaryBuffer;
+
+import java.net.SocketAddress;
+import java.nio.ByteOrder;
 
 public class DesktopPacket extends UniversalPacket {
 
